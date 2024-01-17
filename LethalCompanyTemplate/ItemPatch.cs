0a990674d9ec61af687f4cd2a886079581faf6c5
Loadeksdi <nicolas.load@gmail.com>
2024-01-18 00:14:09 +0100

    WIP

---
+++ LethalCompanyTemplate/ItemPatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine.UIElements;
+
+namespace LethalCompanyTemplate
+{
+    [HarmonyPatch(typeof(GrabbableObject))]
+    public class ItemPatch
+    {
+        [HarmonyPatch(nameof(GrabbableObject.Start))]
+        [HarmonyPostfix]
+        public static void ChangeItemName(GrabbableObject __instance)
+        {
+            if (__instance == null)
+                return;
+
+            string itemName = __instance.itemProperties.name.ToLower();
+            if (itemName == "robot toy")
+                __instance.itemProperties.name = "Arno 2 Poche";
+
+        }
+    }    
+    
+}
--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -1,13 +1,19 @@
-﻿using BepInEx;
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
 
 namespace LethalCompanyTemplate
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        public static String harmonyId = "Arno2poche";
         private void Awake()
         {
             // Plugin startup logic
+            Harmony harmony = new(harmonyId);
+            harmony.PatchAll(typeof(ItemPatch));
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
